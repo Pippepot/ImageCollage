b612b91c7c107755d2d1e99bfb9bdc1c82e3cb12
Philip Thomsen <philip@ubt.dk>
2022-01-02 13:45:00 +0100

    initial commit

---
+++ ImageCollage/Log.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.IO;
+
+namespace ImageCollage
+{
+    static class Log
+    {
+        static string tutorialStep;
+
+        static public void Read()
+        {
+            // Check for viable commands
+            string input;
+            input = Console.ReadLine();
+
+            if (input.ToLower() == "tutorial")
+            {
+                tutorialStep = "conversion-path";
+                Console.WriteLine("\n|TUTORIAL|");
+                Console.WriteLine("Welcome to the tutorial!\n");
+                Console.WriteLine("We will create an image out of many smaller images\n");
+                Console.WriteLine("First we need to prepare the images that make up the bigger image.");
+                Console.WriteLine("To do this, we will make copies of the images in a smaller size and buddy them up with a meta file\n");
+                Console.WriteLine("First type 'conversion-path' followed by the path to a folder with many images, which you would like to create your collage out of.");
+                
+                Read();
+                return;
+            }
+
+            if (input.ToLower() == "help") // help
+            {
+                Console.WriteLine("Further explanation\n");
+                Console.WriteLine("Command name                 Command                         Summary\n");
+                Console.WriteLine("View                         view                            View all saved settings.\n");
+                Console.WriteLine("ConversionPath               conversion-path  'path'         Sets the path of a folder, where a copy of each picture will be convertet.\n");
+                Console.WriteLine("ConversionDestination        conversion-destination  'path'  Sets the path of a folder, where the convertet pictures will be saved to.\n");
+                Console.WriteLine("ImagePath                    image-path  'file'              Sets the path of a file, which will be recreated by convertet pictures.\n");
+                Console.WriteLine("ImageDestination             image-destination  'path'       Sets the path of a folder, where the final image will be saved to.\n");
+                Console.WriteLine("ConvertedSize                size 'number'                   Sets the size of converted images in pixels.Default is 20. If this is changed and images are not reconverted images will be cut off.\n");
+                Console.WriteLine("ImageSections                sections 'number'               Sets the width of recreated images in sections.Default is 80.\n");
+                Console.WriteLine("Override                     override 'true/false'           Overrides any converted image with the same name. Uses conversion path and conversionDestination.\n");
+                Console.WriteLine("Convert                      convert                         Converts image files from conversionPath into smaller collage pieces with meta files and places them in conversionDestination.\n");
+                Console.WriteLine("Create                       create                          Creates a png file in image path by combining convertet images from conversionDestination into a collage located in image destination. Override will not be taken into account. This command will never override existing images.\n");
+
+                Read();
+                return;
+            }
+
+            if (input.ToLower() == "view") // view
+            {
+                Console.WriteLine("Conversion path: " + Properties.AppSettings.Default.ConversionPath);
+                Console.WriteLine("Conversion destination: " + Properties.AppSettings.Default.ConversionDestination);
+                Console.WriteLine("Image path: " + Properties.AppSettings.Default.ImagePath);
+                Console.WriteLine("Image destination: " + Properties.AppSettings.Default.ImageDestination);
+                Console.WriteLine();
+                Console.WriteLine("Conversion size: " + Properties.AppSettings.Default.ConversionSize);
+                Console.WriteLine("Image sections: " + Properties.AppSettings.Default.Sections);
+                Console.WriteLine("Override: " + Properties.AppSettings.Default.Override);
+                Read();
+                return;
+            }
+
+            if (input.ToLower().StartsWith("conversion-path")) // SetConversionPath
+            {
+                string path = input.Substring(("conversion-path").Length);
+
+                // Checks for existance and writes into the settings file
+                if (Directory.Exists(path))
+                {
+                    Properties.AppSettings.Default.ConversionPath = path;
+                    Properties.AppSettings.Default.Save();
+                    LogSucces("Conversion path set");
+
+                    if (tutorialStep == "conversion-path")
+                    {
+                        Console.WriteLine("\n|TUTORIAL|");
+                        Console.WriteLine("Perfect!");
+                        Console.WriteLine("Next type 'conversion-destination' followed by the path to another folder where you would like these new images to be located");
+                        tutorialStep = "conversion-destination";
+                    }
+
+                    Read();
+                }
+                else { LogError("Path does not exist"); }
+                return;
+            }
+
+
+            if (input.ToLower().StartsWith("conversion-destination")) // SetConversionDestination
+            {
+                string path = input.Substring(("conversion-destination").Length);
+
+                // Checks for existance and writes into the settings file
+                if (Directory.Exists(path))
+                {
+                    Properties.AppSettings.Default.ConversionDestination = path;
+                    Properties.AppSettings.Default.Save();
+                    LogSucces("Conversion destination set");
+
+                    if (tutorialStep == "conversion-destination")
+                    {
+                        Console.WriteLine("\n|TUTORIAL|");
+                        Console.WriteLine("Nice!");
+                        Console.WriteLine("Now we need to convert the images to our prefered size by typing 'convert'");
+                        Console.WriteLine("Note that you can change the size of these images by typing 'size' followed by an integer number");
+                        Console.WriteLine("To view what the size currently is, type 'view'");
+                        tutorialStep = "convert";
+                    }
+
+                    Read();
+                }
+                else { LogError("Path does not exist"); }
+                return;
+            }
+
+
+            if (input.ToLower().StartsWith("image-path")) // SetImagePath
+            {
+                // Gets the path after the command ignoring the first space key
+                string path = input.Substring(("image-path").Length);
+
+                // Checks for existance and writes into the settings file
+                if (File.Exists(path))
+                {
+                    Properties.AppSettings.Default.ImagePath = path;
+                    Properties.AppSettings.Default.Save();
+                    LogSucces("Image path set");
+                    if (tutorialStep == "image-path")
+                    {
+                        Console.WriteLine("\n|TUTORIAL|");
+                        Console.WriteLine("Cool!");
+                        Console.WriteLine("Then type 'image-destination' followed by the path the folder where the collage will be placed");
+                        tutorialStep = "image-destination";
+                    }
+                    Read();
+                }
+                else { LogError("File does not exist"); }
+                return;
+            }
+
+
+            if (input.ToLower().StartsWith("image-destination")) // SetImageDestination
+            {
+                // Gets the path after the command ignoring the first space key
+                string path = input.Substring(("image-destination").Length);
+
+                // Checks for existance and writes into the settings file
+                if (Directory.Exists(path))
+                {
+                    Properties.AppSettings.Default.ImageDestination = path;
+                    Properties.AppSettings.Default.Save();
+                    LogSucces("Image destination set");
+                    if (tutorialStep == "image-destination")
+                    {
+                        Console.WriteLine("\n|TUTORIAL|");
+                        Console.WriteLine("This is the final step!");
+                        Console.WriteLine("Type 'create' to make magic happen");
+                        Console.WriteLine("Note you can type 'sections' to change how many converted images make one horizontal line on the collage");
+                        Console.WriteLine("To view how many sections there currently are, type 'view'");
+                        tutorialStep = "create";
+                    }
+                    Read();
+                }
+                else { LogError("Path does not exist"); }
+                return;
+            }
+
+
+            if (input.ToLower().StartsWith("size")) // SetConvertionsize
+            {
+                string size = input.Substring(("size").Length);
+
+                // Writes into the settings file
+                if (int.TryParse(size, out int parse))
+                {
+                    if (parse < 1)
+                    {
+                        LogError("Interger must be greater than 0");
+                        return;
+                    }
+
+                    Properties.AppSettings.Default.ConversionSize = parse;
+                    LogSucces("Size set");
+                    Read();
+                }
+                else { LogError("Missing integer argument"); }
+                return;
+            }
+
+
+            if (input.ToLower().StartsWith("sections")) // Sections
+            {
+                string sectionCount = input.Substring(("sections").Length);
+
+                // Writes into the settings file
+                if (int.TryParse(sectionCount.ToLower(), out int parse))
+                {
+                    if (parse < 1)
+                    {
+                        LogError("Interger must be greater than 0");
+                        return;
+                    }
+
+                    Properties.AppSettings.Default.Sections = parse;
+                    Properties.AppSettings.Default.Save();
+                    LogSucces("Sections set");
+                    Read();
+                }
+                else { LogError("Missing integer argument"); }
+                return;
+            }
+
+
+            if (input.ToLower().StartsWith("override")) // Override
+            {
+                string shouldOverride = input.Substring(("override").Length);
+
+                // Writes into the settings file
+                if (bool.TryParse(shouldOverride.ToLower(), out bool parse))
+                {
+                    Properties.AppSettings.Default.Override = parse;
+                    Properties.AppSettings.Default.Save();
+                    LogSucces("Override set");
+                    Read();
+                }
+                else { LogError("Missing boolean argument"); }
+                return;
+            }
+
+
+            if (input.ToLower() == "convert") // convert
+            {
+                if (!Directory.Exists(Properties.AppSettings.Default.ConversionPath))
+                {
+                    LogError("Unable to find conversion path");
+                    return;
+                }
+
+                if (!Directory.Exists(Properties.AppSettings.Default.ConversionDestination))
+                {
+                    LogError("Unable to find conversion destination");
+                    return;
+                }
+
+                Program.Convert();
+                LogSucces("Images converted");
+
+                if (tutorialStep == "convert")
+                {
+                    Console.WriteLine("\n|TUTORIAL|");
+                    Console.WriteLine("You should now see the converted images in your specified path");
+                    Console.WriteLine("Now we move on the creating the bigger picture\n");
+                    Console.WriteLine("Type 'image-path' followed by the location of the image file that the smaller images will make up");
+                    tutorialStep = "image-path";
+                }
+
+                Read();
+                return;
+            }
+
+
+            if (input.ToLower() == "create") // create
+            {
+                if (!Directory.Exists(Properties.AppSettings.Default.ConversionDestination))
+                {
+                    LogError("Unable to find conversion destination");
+                    return;
+                }
+
+                if (!File.Exists(Properties.AppSettings.Default.ImagePath))
+                {
+                    LogError("Unable to find image file");
+                    return;
+                }
+
+                if (!Directory.Exists(Properties.AppSettings.Default.ImageDestination))
+                {
+                    LogError("Unable to find image destination");
+                    return;
+                }
+
+
+                Program.Create();
+                LogSucces("Image created");
+                if (tutorialStep == "create")
+                {
+                    Console.WriteLine("\n|TUTORIAL|");
+                    Console.WriteLine("Awesome!");
+                    Console.WriteLine("You are all done now!");
+                    Console.WriteLine("Go see you image collage");
+                    Console.WriteLine("If you want to know more type 'help'");
+                    tutorialStep = "";
+                }
+                Read();
+                return;
+            }
+
+            // If input did not match any commands
+            LogError("Unable to recognize '" + input + "'");
+            return;
+        }
+
+        static void LogSucces(string succes)
+        {
+            Console.WriteLine("[SUCCES] " + succes);
+        }
+
+        static void LogError(string error)
+        {
+            Console.WriteLine("[ERROR] " + error);
+            Read();
+        }
+    }
+}
---
+++ ImageCollage/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageCollage
+{
+	class Program
+	{
+
+        static void Main()
+        {
+            Console.Title = "ImageCollage";
+            Console.WriteLine("Type 'tutorial' to get a helping hand");
+            Log.Read();
+        }
+
+        /// <summary>
+        /// Creates png files and meta files storing average RGB value
+        /// </summary>
+        static public void Convert()
+        {
+            // Declare the bitmap array which will be the converted pictures
+            List<Bitmap> pictures = new List<Bitmap>();
+            List<string> picturePaths = new List<string>();
+            string[] filters = new string[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp", "svg" }; // Filter only image files
+            foreach (var filter in filters)
+            {
+                picturePaths.AddRange(Directory.GetFiles(Properties.AppSettings.Default.ConversionPath, string.Format("*.{0}", filter), SearchOption.TopDirectoryOnly));
+            }
+
+            //Create every bitmap with the right size and a metafile to save computing time when creating the entire collage later
+            int j = 0; // Used for whileloop if override is false
+            for (int i = 0; i < picturePaths.Count; i++)
+            {
+                string fileName = Properties.AppSettings.Default.ConversionDestination + @"\conversion " + i + ".png";
+                pictures.Add(new Bitmap(Image.FromFile(picturePaths[i]), new Size(Properties.AppSettings.Default.ConversionSize, Properties.AppSettings.Default.ConversionSize)));
+                // If it should not override existing files, then check for valid filenames so the bitmap does not override
+                if (!Properties.AppSettings.Default.Override)
+                {
+                    while (File.Exists(fileName))
+                    {
+                        j++;
+                        fileName = Properties.AppSettings.Default.ConversionDestination + @"\conversion " + j + ".png";
+                    }
+                }
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        pictures[i].Save(memory, ImageFormat.Jpeg);
+                        byte[] bytes = memory.ToArray();
+                        fs.Write(bytes, 0, bytes.Length);
+                    }
+                }
+
+                // Get the RGB values for the meta file
+                int r = 0;
+                int g = 0;
+                int b = 0;
+
+                for (int y = 0; y < Properties.AppSettings.Default.ConversionSize; y++)
+                {
+                    for (int x = 0; x < Properties.AppSettings.Default.ConversionSize; x++)
+                    {
+                        r += pictures[i].GetPixel(x, y).R * pictures[i].GetPixel(x, y).R;
+                        g += pictures[i].GetPixel(x, y).G * pictures[i].GetPixel(x, y).G;
+                        b += pictures[i].GetPixel(x, y).B * pictures[i].GetPixel(x, y).B;
+                    }
+                }
+                int pixels = Properties.AppSettings.Default.ConversionSize * Properties.AppSettings.Default.ConversionSize;
+                r = (int)Math.Sqrt(r / pixels);
+                g = (int)Math.Sqrt(g / pixels);
+                b = (int)Math.Sqrt(b / pixels);
+                File.WriteAllLines(fileName + ".meta", new string[] { r.ToString(), g.ToString(), b.ToString() } );
+
+                Console.WriteLine((int)((float)i * 100 / (float)(picturePaths.Count * Properties.AppSettings.Default.Sections)) + "%");
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+            }
+        }
+
+
+        /// <summary>
+        /// Creates a png file by combining convertet images into a collage
+        /// </summary>
+        static public void Create()
+        {
+            Console.WriteLine("Creating Image...");
+            // Gets the converted images as a palete
+            List<Bitmap> convertedImages = new List<Bitmap>();
+            List<string> convertedImagesPath = new List<string>();
+            convertedImagesPath.AddRange(Directory.GetFiles(Properties.AppSettings.Default.ConversionDestination, string.Format("*.{0}", "png"), SearchOption.TopDirectoryOnly));
+            Color[] convetedImagesRGB = new Color[convertedImagesPath.Count];
+            for (int i = 0; i < convertedImagesPath.Count; i++)
+            {
+                convertedImages.Add(new Bitmap(Image.FromFile(convertedImagesPath[i]))); // Get bitmapfiles
+                string[] metaString = File.ReadAllLines(convertedImagesPath[i] + ".meta");
+                convetedImagesRGB[i] = Color.FromArgb(int.Parse(metaString[0]), int.Parse(metaString[1]), int.Parse(metaString[2]));
+            }
+
+            // Declare the image which will be recreated
+            Bitmap originalImage = new Bitmap(Image.FromFile(Properties.AppSettings.Default.ImagePath));
+            int sectionsWide = (int)Math.Ceiling(Properties.AppSettings.Default.Sections * ((float)originalImage.Height / (float)originalImage.Width));
+
+            // The final image which will be written to file as png
+            Bitmap recreatedImage = new Bitmap(originalImage, Properties.AppSettings.Default.ConversionSize * Properties.AppSettings.Default.Sections, Properties.AppSettings.Default.ConversionSize * sectionsWide);
+
+            // Lock the bitmap bits to memory
+            Rectangle rect = new Rectangle(0, 0, recreatedImage.Width, recreatedImage.Height);
+            BitmapData bmpData = recreatedImage.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            // Get address of the first line in memory aka. Array[0]
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * recreatedImage.Height;
+            byte[] rgbValues = new byte[bytes];
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+
+            // MAINPULATE THE IMAGE
+            // One pixel in the 1-dimesional byte array is 3 bytes, therefore many values will be multiplied by 3.
+
+            int row = 0;
+            // For every section
+            for (int section = 0; section < Properties.AppSettings.Default.Sections * sectionsWide; section++)
+            {
+                // If width is reached, jump to next row
+                if (section != 0 && section % Properties.AppSettings.Default.Sections == 0)
+                {
+                    row += rect.Width * Properties.AppSettings.Default.ConversionSize * 3 - Properties.AppSettings.Default.Sections * Properties.AppSettings.Default.ConversionSize * 3;
+                    Console.WriteLine((int)((float)section * 100 / (float)(sectionsWide * Properties.AppSettings.Default.Sections)) + "%");
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                }
+                int pixel = section * Properties.AppSettings.Default.ConversionSize * 3 + row;
+
+                // Get the RGB values for one section of the original image
+                int r = 0;
+                int g = 0;
+                int b = 0;
+
+                for (int y = 0; y < Properties.AppSettings.Default.ConversionSize; y++)
+                {
+                    for (int x = pixel; x < pixel + Properties.AppSettings.Default.ConversionSize * 3; x += 3) // *3 because each pixel has 3 values
+                    {
+                        r += rgbValues[2 + x] * rgbValues[2 + x];
+                        g += rgbValues[1 + x] * rgbValues[1 + x];
+                        b += rgbValues[x] * rgbValues[x];
+
+                    }
+                    pixel += rect.Width * 3;
+                }
+
+                pixel = section * Properties.AppSettings.Default.ConversionSize * 3 + row;
+
+                r = (int)Math.Sqrt(r / (Properties.AppSettings.Default.ConversionSize * Properties.AppSettings.Default.ConversionSize));
+                g = (int)Math.Sqrt(g / (Properties.AppSettings.Default.ConversionSize * Properties.AppSettings.Default.ConversionSize));
+                b = (int)Math.Sqrt(b / (Properties.AppSettings.Default.ConversionSize * Properties.AppSettings.Default.ConversionSize));
+                // Compare the average value of one section of the original image's average RGB value
+                int[] distance = new int[convertedImages.Count];
+                for (int i = 0; i < convertedImages.Count; i++) // Find distance
+                {
+                    //Do some math to make compare the colors as a human eye would compare colors
+                    int multiplier = (r + convetedImagesRGB[i].R) / 2;
+                    distance[i] = (int)Math.Sqrt((2 + multiplier / 256) * (r - convetedImagesRGB[i].R) * (r - convetedImagesRGB[i].R)
+                        + 4 * (g - convetedImagesRGB[i].G) * (g - convetedImagesRGB[i].G)
+                        + (2 + (255 - multiplier) / 256) * (b - convetedImagesRGB[i].B) * (b - convetedImagesRGB[i].B));
+                }
+                // Find the image with the closest RGB value
+                Bitmap closestBmp = convertedImages[Array.IndexOf(distance, distance.Min())];
+
+                // Write the closest image to a variable
+                for (int y = 0; y < Properties.AppSettings.Default.ConversionSize; y++)
+                {
+                    int indexX = 0;
+                    for (int x = pixel; x < pixel + Properties.AppSettings.Default.ConversionSize * 3; x += 3) // *3 because each pixel has 3 values
+                    {
+                        byte red = closestBmp.GetPixel(indexX, y).R;
+                        byte green = closestBmp.GetPixel(indexX, y).G;
+                        byte blue = closestBmp.GetPixel(indexX, y).B;
+                        rgbValues[x + 2] = red;
+                        rgbValues[x + 1] = green;
+                        rgbValues[x] = blue;
+                        indexX++;
+                    }
+
+                    pixel += rect.Width * 3;
+                }
+                // Continues with the next section until the recreated image is complete
+            }
+
+            // Copy the RGB values into memory, overriding the original image
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            // Unlock the bytes from memory
+            recreatedImage.UnlockBits(bmpData);
+
+            // Save the recreated image to file
+            int j = 0;
+            string fileName = Properties.AppSettings.Default.ImageDestination + @"\Recreated.png";
+            // Makes sure no file will be overwritten
+            while (File.Exists(fileName))
+            {
+                j++;
+                fileName = Properties.AppSettings.Default.ImageDestination + @"\Recreated " + j + ".png";
+            }
+
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    recreatedImage.Save(memory, ImageFormat.Png);
+                    byte[] imageBytes = memory.ToArray();
+                    fs.Write(imageBytes, 0, imageBytes.Length);
+                }
+            }
+        }
+    }
+}
+
